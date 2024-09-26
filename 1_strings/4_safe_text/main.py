1afe4ba1f5af16f6b24c1d907c8567e65bd099f1
BDC078 <dasaudina97@gmail.com>
2024-09-26 17:39:24 +0300

    comleted 4_safe_text

--- 1_strings/4_safe_text/main.py
+++ 1_strings/4_safe_text/main.py
@@ -1,4 +1,6 @@
 import os
+from audioop import reverse
+from os.path import split
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SPLIT_SYMBOL = '.\n'
@@ -22,4 +24,18 @@ def recover_article() -> str:
     wrong_article = get_wrong_article()
 
     # Ваш код ниже, возвращайте уже отредактированный текст!
-    return wrong_article
+    split_article = wrong_article.splitlines()
+    correct_sentence = ''
+    correct_article = ''
+    for sentence in split_article:
+        sentence_without_exc_marks = sentence[:len(sentence)//2].lower()[::-1]
+        split_sentence = sentence_without_exc_marks.split()
+        for word in split_sentence:
+            find_woof = word.find("woof-woof")
+            if find_woof == -1:
+                correct_sentence = correct_sentence + word + ' '
+            else:
+                correct_sentence = correct_sentence + word[:find_woof] + 'cat' + word[find_woof + 9:] + " "
+        correct_article = correct_article + correct_sentence[:len(correct_sentence)-1].capitalize() + SPLIT_SYMBOL
+        correct_sentence = ''
+    return correct_article
