be1dfc9bbf8439a496a16246b630a3cc17c0aa8f
BDC078 <dasaudina97@gmail.com>
2024-10-09 16:15:44 +0300

    completed 1_task

--- 2_dicts/1_task/main.py
+++ 2_dicts/1_task/main.py
@@ -1,4 +1,7 @@
 import os
+from os.path import split
+
+from decimal import Decimal
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SPLIT_SYMBOL = '\n'
@@ -19,8 +22,20 @@ def get_employees_info() -> list[str]:
 
 def get_parsed_employees_info() -> list[dict[str, int | str]]:
     """Функция парсит данные, полученные из внешнего API и приводит их к стандартизированному виду."""
-    _ = get_employees_info()
+    info = get_employees_info()
     parsed_employees_info = []
 
     # Ваш код ниже
+    for sentence in info:
+        dict = {'id': 0, 'name': 0, 'last_name': 0, 'age': 0, 'position': 0, 'salary': 0}
+        split_sentence = sentence.split()
+        for index in range(len(split_sentence)):
+            if split_sentence[index] in ['id','age']:
+                dict[split_sentence[index]] = int(split_sentence[index + 1])
+            elif split_sentence[index] in ['name', 'last_name', 'position']:
+                dict[split_sentence[index]] = str(split_sentence[index + 1])
+            elif split_sentence[index] in 'salary':
+                dict[split_sentence[index]] = Decimal(split_sentence[index + 1])
+        parsed_employees_info += [dict]
     return parsed_employees_info
+
