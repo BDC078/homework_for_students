65f5596093a48f4ff0882703222552dc90e7a4af
BDC078 <dasaudina97@gmail.com>
2024-10-09 18:19:53 +0300

    completed 3_task

--- 2_dicts/3_task/main.py
+++ 2_dicts/3_task/main.py
@@ -1,12 +1,18 @@
+from os.path import split
+
+
 def format_phone(phone_number: str) -> dict[str, int]:
-    """Функция возвращает отформатированный телефон.
-
-    Args:
-        phone_number: исходный телефон
-
-    Returns:
-        отформатированный телефон
-    """
-    formatted_phone_number = ""
-
-    return formatted_phone_number
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+']
+    correct_phone_numbers = ''
+    for index in range(len(phone_number)):
+        if phone_number[index] in numbers:
+            correct_phone_numbers += phone_number[index]
+    if correct_phone_numbers[0] == '7' or correct_phone_numbers[0] == '8':
+        return ('8 (' + correct_phone_numbers[1:4]+') '+correct_phone_numbers[4:7]+'-'+correct_phone_numbers[7:9] +
+                '-' + correct_phone_numbers[9:11])
+    elif correct_phone_numbers[0] == '+':
+        return ('8 ('+correct_phone_numbers[2:5]+') '+correct_phone_numbers[5:8]+'-'+correct_phone_numbers[8:10] +
+              '-'+ correct_phone_numbers[10:12])
+    elif correct_phone_numbers[0] == '9':
+        return ('8 ('+correct_phone_numbers[0:3]+') '+ correct_phone_numbers[3:6]+ '-'+ correct_phone_numbers[6:8]+
+              '-'+correct_phone_numbers[8:10])
