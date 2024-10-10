4468dd94b91916973f3fa1d0262397439f80813a
BDC078 <dasaudina97@gmail.com>
2024-10-10 12:33:24 +0300

    completed 1_phone_text_code

--- 3_functions/1_phone_text_code/decode/decode.py
+++ 3_functions/1_phone_text_code/decode/decode.py
@@ -1,4 +1,25 @@
 def decode_numbers(numbers: str) -> str | None:
     """Пишите ваш код здесь."""
-    result = ""
+    result = ''
+    list_numbers = numbers.lower().split()
+    keyboard_dict = {
+        '1': ['.', ',', '?', '!', ':', ';'],
+        '2': ['а', 'б', 'в', 'г'],
+        '3': ['д', 'е', 'ж', 'з'],
+        '4': ['и', 'й', 'к', 'л'],
+        '5': ['м', 'н', 'о', 'п'],
+        '6': ['р', 'с', 'т', 'у'],
+        '7': ['ф', 'х', 'ц', 'ч'],
+        '8': ['ш', 'щ', 'ъ', 'ы'],
+        '9': ['ь', 'э', 'ю', 'я'],
+        '0': [' ']
+    }
+    for code in list_numbers:
+        if (((code[0] == '1') and len(code) <= 6) or ((code[0] in ['2', '3', '4', '5', '6', '7', '8', '9'])
+                                                      and len(code) <= 4)
+            or ((code[0] == '0') and len(code) <= 1)) and (code[0] == code[-1]):
+            result += keyboard_dict[code[0]][len(code) - 1]
+        else:
+            result = None
+            break
     return result
--- 3_functions/1_phone_text_code/encode/encode.py
+++ 3_functions/1_phone_text_code/encode/encode.py
@@ -1,4 +1,25 @@
 def encode_text(text: str) -> str | None:
     """Пишите ваш код здесь."""
-    result = ""
-    return result
+    keyboard_dict_swap = {}
+    keyboard_dict = {
+        '1': ['.', ',', '?', '!', ':', ';'],
+        '2': ['а', 'б', 'в', 'г'],
+        '3': ['д', 'е', 'ж', 'з'],
+        '4': ['и', 'й', 'к', 'л'],
+        '5': ['м', 'н', 'о', 'п'],
+        '6': ['р', 'с', 'т', 'у'],
+        '7': ['ф', 'х', 'ц', 'ч'],
+        '8': ['ш', 'щ', 'ъ', 'ы'],
+        '9': ['ь', 'э', 'ю', 'я'],
+        '0': [' ']
+    }
+    for key, value in keyboard_dict.items():
+        for elem in range(len(value)):
+            keyboard_dict_swap[value[elem]] = key * (elem + 1)
+    result = ''
+    for word in text:
+        if word in keyboard_dict_swap:
+            result += keyboard_dict_swap[word] + ' '
+        else:
+            return None
+    return result[:-1]
