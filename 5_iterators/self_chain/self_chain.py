e49891d4d4e32241d882aeedd269d1c22b641329
BDC078 <dasaudina97@gmail.com>
2024-11-17 22:46:06 +0300

    completed self_chain

--- 5_iterators/self_chain/self_chain.py
+++ 5_iterators/self_chain/self_chain.py
@@ -5,8 +5,19 @@ T = TypeVar("T")
 
 def chain(*iterables: Iterable[T]) -> Generator[T, None, None]:
     """Пишите ваш код здесь"""
-
+    for iter in iterables:
+        for elem in iter:
+            yield elem
 
 class Chain:
     def __init__(self, *iterables: Iterable[T]):
         """Реализуйте класс ниже"""
+        self.iter = iterables
+
+    def __iter__(self):
+        for iter in self.iter:
+            for elem in iter:
+                yield elem
+
+    def __next__(self):
+        return self
