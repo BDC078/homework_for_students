c21182425be7cadefac177109eb1aa3b67534b9e
BDC078 <dasaudina97@gmail.com>
2024-11-17 21:19:09 +0300

    completed retry and fixed test_retry

--- 4_decorators/retry/retry.py
+++ 4_decorators/retry/retry.py
@@ -1,2 +1,19 @@
 # реализуйте декоратор вида @retry(count: int, delay: timedelta, handled_exceptions: tuple[type(Exceptions)])
+import time
+from datetime import timedelta
 
+
+def retry(count, delay, handled_exceptions = Exception):
+    if count < 1:
+        raise ValueError
+    def decorator(func):
+        def wrapper(*args, **dict):
+            for i in range(count):
+                try:
+                    return func(*args, **dict)
+                except handled_exceptions:
+                    if i + 1 == count:
+                        raise
+                time.sleep(delay.total_seconds())
+        return wrapper
+    return decorator
