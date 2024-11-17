b5b1fe4759e2dd954159e3291470696d9e4f5a6d
BDC078 <dasaudina97@gmail.com>
2024-11-17 22:42:29 +0300

    completed self_cycle

--- 5_iterators/self_cycle/self_cycle.py
+++ 5_iterators/self_cycle/self_cycle.py
@@ -5,8 +5,19 @@ T = TypeVar("T")
 
 def cycle(obj: Iterable[T]) -> Generator[T, None, None]:
     """Пишите ваш код здесь."""
-
+    while True:
+        for value in obj:
+            yield value
 
 class Cycle:
     def __init__(self, obj: Iterable[T]):
         """Реализуйте класс"""
+        self.obj = obj
+
+    def __iter__(self):
+        while True:
+            for i in self.obj:
+                yield i
+
+    def __next__(self):
+        return self
